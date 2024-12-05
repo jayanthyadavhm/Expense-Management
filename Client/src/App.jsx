ffe866ee44643a639655a22c73e975b1360b08e8
channrama <dschannappa93@gmail.com>
2024-12-05 19:07:18 +0530

    created setup

---
+++ Client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import {Routes,Route} from "react-router-dom"
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+function App() {
+ 
+
+  return (
+    <>
+   <Routes>
+     <Route path='/' element={<Home/>}/> 
+     <Route path='/about' element={<About/>}/> 
+
+   </Routes>
+    </>
+  )
+}
+
+export default App
